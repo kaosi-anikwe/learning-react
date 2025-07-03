0bfdf863dcb659754f11f7673529d64286505903
Kaosi Anikwe <anikwehenryasa@gmail.com>
2025-07-03 18:41:59 +0100

    Implement core components for React Quiz application

---
+++ 10-react-quiz/src/components/Quiz.jsx
@@ -0,0 +1,38 @@
+import Question from "./Question";
+import QUESTIONS from "../questions";
+import { useCallback, useState } from "react";
+import Summary from "./Summary";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return <Summary userAnswers={userAnswers} />;
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+}
