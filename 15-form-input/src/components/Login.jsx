f8dedbb1d8ae6d760143b274d2c9222545d8bf5b
Kaosi Anikwe <anikwehenryasa@gmail.com>
2025-07-09 23:26:05 +0100

    feat: add header and signup form components with validation utilities

--- 15-form-input/src/components/Login.jsx
+++ 15-form-input/src/components/Login.jsx
@@ -1,17 +1,44 @@
+import { useRef, useState } from "react";
+
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    setEmailIsInvalid(false);
+
+    console.log("Sending HTTP Request...");
+    console.log(enteredEmail, enteredPassword);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
