d15b3cdd0b08b9509271fa114111dd6a09bbd1f9
Kaosi Anikwe <anikwehenryasa@gmail.com>
2025-07-11 02:12:45 +0100

    starting project

---
+++ 17-async-form-actions/eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js, "react-hooks": pluginReactHooks },
+    extends: ["js/recommended"],
+  },
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: { version: "detect" },
+    },
+  },
+]);
