4e2ec442f89b5c80d0abd868be07ad547df2c5a3
Kaosi Anikwe <anikwehenryasa@gmail.com>
2025-07-16 13:49:55 +0100

    feat: implement authentication flow with signup, login, and token management

--- 22-authentication/frontend/src/pages/Root.js
+++ 22-authentication/frontend/src/pages/Root.js
@@ -1,15 +1,39 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "POST" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    const timeout = setTimeout(() => {
+      submit(null, { action: "/logout", method: "POST" });
+    }, tokenDuration);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [submit, token]);
 
   return (
     <>
       <MainNavigation />
       <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
