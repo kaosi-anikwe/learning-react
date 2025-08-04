861fc5da6c4a82c544497f7c7e59fe2d2e882350
Kaosi Anikwe <anikwehenryasa@gmail.com>
2025-08-04 16:19:18 +0100

    feat: implement UsePromiseDemo component for async data fetching and update Home page

--- 25-rsc-server-actions/app/page.js
+++ 25-rsc-server-actions/app/page.js
@@ -1,10 +1,22 @@
-import DataFetchingDemo from "@/components/DataFetchingDemo";
-import ServerActionsDemo from "@/components/ServerActionsDemo";
+import { Suspense } from "react";
+import fs from "node:fs/promises";
 
-export default function Home() {
+import UsePromiseDemo from "@/components/UsePromisesDemo";
+
+export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      resolve(users);
+    }, 2000)
+  );
+
   return (
     <main>
-      <ServerActionsDemo />
+      <Suspense fallback={<p>Loading users...</p>}>
+        <UsePromiseDemo usersPromise={fetchUsersPromise} />
+      </Suspense>
     </main>
   );
 }
