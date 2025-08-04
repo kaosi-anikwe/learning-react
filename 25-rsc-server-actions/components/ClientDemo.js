a089a6520154c5b5378450d708a7edde88e467a8
Kaosi Anikwe <anikwehenryasa@gmail.com>
2025-08-04 15:47:05 +0100

    feat: implement DataFetchingDemo component and update Home page to use it

--- 25-rsc-server-actions/components/ClientDemo.js
+++ 25-rsc-server-actions/components/ClientDemo.js
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function ClientDemo({ children }) {
   const [count, setCount] = useState(0); // <- this is why it's a client component
 
-  console.log('ClientDemo rendered');
+  console.log("ClientDemo rendered");
   return (
     <div className="client-cmp">
       <h2>A React Client Component</h2>
