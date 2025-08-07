464190239b4aec37ead02c1e48a8958d98634170
Kaosi Anikwe <anikwehenryasa@gmail.com>
2025-08-07 14:59:19 +0100

    feat: implement Accordion component with context for managing open items and update App to use it

---
+++ 27-practices/src/components/Accordion/AccordionItem.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+const AccordionItemContext = createContext();
+
+export function useAccordionItemContext() {
+  const ctx = useContext(AccordionItemContext);
+
+  if (!ctx) {
+    throw new Error(
+      "AccordionItem-related components must be wrapped with <AccordionItem>"
+    );
+  }
+
+  return ctx;
+}
+
+export default function AccordionItem({ id, children, className }) {
+  return (
+    <AccordionItemContext.Provider value={id}>
+      <li className={className}>{children}</li>
+    </AccordionItemContext.Provider>
+  );
+}
